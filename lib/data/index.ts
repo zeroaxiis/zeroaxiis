0314d712244574ff084cc793ef091e8a11b982ed
Atomic-Joy <mjoy220804@gmail.com>
2026-06-09 22:54:42 +0530

    feat: migrate project data to JSON and add SelectedWork and OpenSource section components

--- lib/data/index.ts
+++ lib/data/index.ts
@@ -1,11 +1,15 @@
+import type { Project, OpenSourceTool, Contributor, ProjectCategory } from "@/types";
+import projectsData from "./projects.json";
+
 export { services } from "./services";
 export type { Service } from "@/types";
 
 export { workflowSteps } from "./workflow";
 export type { WorkflowStep } from "@/types";
 
-export { projects, openSourceTools } from "./projects";
-export type { Project, OpenSourceTool, Contributor, ProjectCategory } from "@/types";
+export const projects = projectsData.projects as Project[];
+export const openSourceTools = projectsData.openSourceTools as OpenSourceTool[];
+export type { Project, OpenSourceTool, Contributor, ProjectCategory };
 
 export { teamMembers, contactItems } from "./team";
 export type { TeamMember, ContactItem } from "@/types";
